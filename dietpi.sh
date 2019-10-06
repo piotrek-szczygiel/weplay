da2bed6330c8d8aba9f99c32f6bca0d3874ba7db
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-10-06 03:15:21 +0200

    Add AP config to dietpi.sh

--- dietpi.sh
+++ dietpi.sh
@@ -2,5 +2,39 @@
 set -e
 
 apt update
-apt install libraspberrypi-dev raspberrypi-kernel-headers build-essential cmake
+apt install libraspberrypi-dev raspberrypi-kernel-headers build-essential cmake hostapd dnsmasq iptables
 usermod -aG video dietpi
+
+cat << EOF > /etc/create_ap.conf
+CHANNEL=default
+GATEWAY=192.168.12.1
+WPA_VERSION=1+2
+ETC_HOSTS=0
+DHCP_DNS=gateway
+NO_DNS=0
+NO_DNSMASQ=0
+HIDDEN=0
+MAC_FILTER=0
+MAC_FILTER_ACCEPT=/etc/hostapd/hostapd.accept
+ISOLATE_CLIENTS=0
+SHARE_METHOD=none
+IEEE80211N=0
+IEEE80211AC=0
+HT_CAPAB=[HT40+]
+VHT_CAPAB=
+DRIVER=nl80211
+NO_VIRT=0
+COUNTRY=
+FREQ_BAND=2.4
+NEW_MACADDR=
+DAEMONIZE=0
+NO_HAVEGED=0
+WIFI_IFACE=wlan0
+INTERNET_IFACE=
+SSID=RaspberryConsole
+PASSPHRASE=Korobeiniki1984
+USE_PSK=0
+EOF
+
+systemctl start create_ap
+systemctl enable create_ap
