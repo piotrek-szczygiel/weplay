3dccb8e33c946a85c9a1be51ad5032abc345c676
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-10-09 19:39:34 +0200

    Fix flying camera

--- console/demo/demo.go
+++ console/demo/demo.go
@@ -16,7 +16,7 @@ type Demo struct {
 	target rl.RenderTexture2D
 
 	camera      rl.Camera
-	cameraAngle [2]float64
+	cameraAngle [3]float64
 	pressed     [3]bool
 }
 
@@ -37,13 +37,13 @@ func New() *Demo {
 		}
 	}
 
-	d.target = rl.LoadRenderTexture(3000, 2000)
+	d.target = rl.LoadRenderTexture(1920, 1080)
 	rl.SetTextureFilter(d.target.Texture, rl.FilterAnisotropic16x)
 
 	d.camera.Position = rl.Vector3{0, 5, 0}
-	d.camera.Target = rl.Vector3{0, 5, 1}
-	d.camera.Up = rl.Vector3{0, 2, 0}
-	d.camera.Fovy = 90
+	d.camera.Target = rl.Vector3{0, 0, 0}
+	d.camera.Up = rl.Vector3{0, 1, 0}
+	d.camera.Fovy = 60
 	d.camera.Type = rl.CameraPerspective
 
 	rl.SetCameraMode(d.camera, rl.CameraCustom)
@@ -61,6 +61,7 @@ func (d *Demo) Update(events []controller.Event) {
 		}
 	}
 
+	dt := float64(rl.GetFrameTime())
 	var left, front, right, up, down bool
 
 	if d.pressed[controller.Left] || rl.IsKeyDown(rl.KeyLeft) {
@@ -83,8 +84,7 @@ func (d *Demo) Update(events []controller.Event) {
 		down = true
 	}
 
-	dt := float64(rl.GetFrameTime())
-	d.updateCamera(20*dt, left, front, right, up, down)
+	d.updateCamera(dt, left, front, right, up, down)
 }
 
 func (d *Demo) Draw() {
@@ -108,10 +108,11 @@ func (d *Demo) GetTarget() rl.RenderTexture2D {
 	return d.target
 }
 
-func (d *Demo) updateCamera(speed float64, left, front, right, up, down bool) {
+func (d *Demo) updateCamera(dt float64, left, front, right, up, down bool) {
 	const (
-		Sensitivity   = 0.2
+		Sensitivity   = 0.4
 		FocusDistance = 25
+		TiltSpeed     = 3
 	)
 
 	var f, h, v float64
@@ -131,18 +132,62 @@ func (d *Demo) updateCamera(speed float64, left, front, right, up, down bool) {
 		v = -1
 	}
 
-	offsetX := speed * -math.Sin(d.cameraAngle[0]) * f
-	offsetY := speed * math.Sin(d.cameraAngle[1]) * f
-	offsetZ := speed * -math.Cos(d.cameraAngle[0]) * f
+	offsetX := 20 * dt * -math.Sin(d.cameraAngle[0]) * f
+	offsetY := 20 * dt * math.Sin(d.cameraAngle[1]) * f
+	offsetZ := 20 * dt * -math.Cos(d.cameraAngle[0]) * f
 
 	d.camera.Position.X += float32(offsetX)
 	d.camera.Position.Y += float32(offsetY)
 	d.camera.Position.Z += float32(offsetZ)
 
-	d.cameraAngle[0] += speed * h * Sensitivity
-	d.cameraAngle[1] += speed * v * Sensitivity
+	d.cameraAngle[0] += 10 * dt * h * Sensitivity
+	d.cameraAngle[1] += 10 * dt * v * Sensitivity
+
+	if d.cameraAngle[0] > 2*math.Pi {
+		d.cameraAngle[0] -= 2 * math.Pi
+	} else if d.cameraAngle[0] < 2*math.Pi {
+		d.cameraAngle[0] += 2 * math.Pi
+	}
+
+	if d.cameraAngle[1] > math.Pi/3 {
+		d.cameraAngle[1] = math.Pi / 3
+	} else if d.cameraAngle[1] < -math.Pi/3 {
+		d.cameraAngle[1] = -math.Pi / 3
+	}
 
 	d.camera.Target.X = d.camera.Position.X - float32(math.Sin(d.cameraAngle[0])*FocusDistance)
 	d.camera.Target.Y = d.camera.Position.Y + float32(math.Sin(d.cameraAngle[1])*FocusDistance)
 	d.camera.Target.Z = d.camera.Position.Z - float32(math.Cos(d.cameraAngle[0])*FocusDistance)
+
+	dir := rl.Vector3{
+		(d.camera.Target.X - d.camera.Position.X) / FocusDistance,
+		(d.camera.Target.Y - d.camera.Position.Y) / FocusDistance,
+		(d.camera.Target.Z - d.camera.Position.Z) / FocusDistance,
+	}
+
+	tiltOffset := dt * TiltSpeed
+
+	if left {
+		d.cameraAngle[2] -= tiltOffset
+	} else if right {
+		d.cameraAngle[2] += tiltOffset
+	} else {
+		if d.cameraAngle[2] > tiltOffset {
+			d.cameraAngle[2] -= tiltOffset
+		} else if d.cameraAngle[2] < -tiltOffset {
+			d.cameraAngle[2] += tiltOffset
+		}
+	}
+
+	if d.cameraAngle[2] > 1.0 {
+		d.cameraAngle[2] = 1.0
+	} else if d.cameraAngle[2] < -1.0 {
+		d.cameraAngle[2] = -1.0
+	}
+
+	a := float32(d.cameraAngle[2] * -math.Pi / 8)
+
+	d.camera.Up.X = a*dir.Z - dir.X*dir.Y
+	d.camera.Up.Y = dir.X*dir.X + dir.Z*dir.Z
+	d.camera.Up.Z = -dir.Y*dir.Z - a*dir.X
 }
