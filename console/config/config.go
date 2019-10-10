3c7697eef30709e24efbbf956ae8c7fa61b33551
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-10-10 22:19:49 +0200

    Add default config, config saving

--- console/config/config.go
+++ console/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/BurntSushi/toml"
 )
@@ -24,18 +25,54 @@ type Controller struct {
 	Address string
 }
 
-func New(filename string) (config Config) {
+func New(filename string) Config {
+	config, err := Load(filename)
+	if err != nil {
+		config = Config{
+			Console{
+				Resolution: [2]int32{1920, 1080},
+				Fullscreen: false,
+				Stretch:    false,
+				VSync:      true,
+				ShowFPS:    true,
+			},
+			Controller{
+				Address: "127.0.0.1:1984",
+			},
+		}
+
+		config.Save(filename)
+	}
+
+	return config
+}
+
+func Load(filename string) (Config, error) {
 	configData, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Println("Unable to read", filename)
-		return
+		return Config{}, err
 	}
 
+	var config Config
 	if _, err := toml.Decode(string(configData), &config); err != nil {
 		log.Println("Unable to decode", filename)
-		return
+		return Config{}, err
 	}
 
 	log.Printf("Loaded config: %+v\n", config)
-	return
+	return config, nil
+}
+
+func (config Config) Save(filename string) {
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Printf("Error while saving config to '%s': %v\n", filename, err)
+		return
+	}
+
+	t := toml.NewEncoder(f)
+	if err := t.Encode(config); err != nil {
+		log.Println("Error while encoding config:", err)
+	}
 }
