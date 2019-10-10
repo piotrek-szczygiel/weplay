73645f3c62609e77487f060dade64cd2aa3967f8
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-10-10 21:57:47 +0200

    Implement all camera operations

--- console/demo/demo.go
+++ console/demo/demo.go
@@ -1,13 +1,12 @@
 package demo
 
 import (
-	"math"
-
+	math "github.com/chewxy/math32"
 	rl "github.com/gen2brain/raylib-go/raylib"
 	"github.com/piotrek-szczygiel/raspberry-console/console/controller"
 )
 
-const maxColumns = 2000
+const maxColumns = 1000
 
 type Demo struct {
 	heights   [maxColumns]float32
@@ -17,19 +16,17 @@ type Demo struct {
 	target rl.RenderTexture2D
 
 	camera      rl.Camera
-	cameraAngle float64
+	cameraAngle rl.Vector3
 	pressed     [3]bool
 
 	renderDistance float32
-
-	forwardSpeed float64
-	turningSpeed float64
 }
 
 func New() *Demo {
 	var demo Demo
+
 	for i := 0; i < maxColumns; i++ {
-		demo.heights[i] = float32(rl.GetRandomValue(3, 20))
+		demo.heights[i] = float32(rl.GetRandomValue(6, 30))
 		demo.positions[i] = rl.Vector3{
 			X: float32(rl.GetRandomValue(50, 10000)),
 			Y: demo.heights[i] / 2,
@@ -43,18 +40,19 @@ func New() *Demo {
 		}
 	}
 
-	demo.target = rl.LoadRenderTexture(1920, 1080)
-	rl.SetTextureFilter(demo.target.Texture, rl.FilterAnisotropic16x)
+	demo.target = rl.LoadRenderTexture(int32(rl.GetScreenWidth()), int32(rl.GetScreenHeight()))
+	rl.SetTextureFilter(demo.target.Texture, rl.FilterAnisotropic4x)
 
-	demo.camera.Position = rl.Vector3{25, 5, 0}
-	demo.camera.Target = rl.Vector3{0, 0, 0}
-	demo.camera.Up = rl.Vector3{0, 1, 0}
+	demo.camera.Position = rl.Vector3{X: 25, Y: 5}
+	demo.camera.Target = rl.Vector3{}
+	demo.camera.Up = rl.Vector3{Y: 1}
 	demo.camera.Fovy = 60
 	demo.camera.Type = rl.CameraPerspective
 
-	demo.cameraAngle = -math.Pi / 2
+	demo.cameraAngle.X = -math.Pi / 2
 
 	demo.renderDistance = 500
+	rl.SetCameraMode(demo.camera, rl.CameraCustom)
 
 	return &demo
 }
@@ -69,77 +67,74 @@ func (demo *Demo) Update(events []controller.Event) {
 		}
 	}
 
-	dt := float64(rl.GetFrameTime())
+	dt := rl.GetFrameTime()
+	horizontalSpeed := dt * 50
+	verticalSpeed := dt * 25
+	rotationSpeed := dt * 2
 
-	var left, front, right bool
-	if demo.pressed[controller.Left] || rl.IsKeyDown(rl.KeyLeft) {
-		left = true
+	var back, front, left, right, up, down float32
+
+	if rl.IsKeyDown(rl.KeyS) {
+		back = horizontalSpeed
 	}
 
 	if demo.pressed[controller.Middle] || rl.IsKeyDown(rl.KeyW) {
-		front = true
+		front = horizontalSpeed
 	}
 
-	if demo.pressed[controller.Right] || rl.IsKeyDown(rl.KeyRight) {
-		right = true
+	if demo.pressed[controller.Left] || rl.IsKeyDown(rl.KeyA) {
+		left = horizontalSpeed
 	}
 
-	const (
-		forwardAcc = 75
-		turningAcc = 3
-		maxTurn    = 1
-	)
-
-	if front {
-		demo.forwardSpeed += dt * forwardAcc
-		if demo.forwardSpeed > forwardAcc {
-			demo.forwardSpeed = forwardAcc
-		}
-	} else {
-		demo.forwardSpeed -= dt * 10
-		if demo.forwardSpeed < 0 {
-			demo.forwardSpeed = 0
-		}
+	if demo.pressed[controller.Right] || rl.IsKeyDown(rl.KeyD) {
+		right = horizontalSpeed
 	}
 
-	if left && !right {
-		demo.turningSpeed += dt * turningAcc
-		if demo.turningSpeed > maxTurn {
-			demo.turningSpeed = maxTurn
-		}
-	} else if right && !left {
-		demo.turningSpeed -= dt * turningAcc
-		if demo.turningSpeed < -maxTurn {
-			demo.turningSpeed = -maxTurn
-		}
-	} else if demo.turningSpeed < -dt*turningAcc*2 {
-		demo.turningSpeed += dt * turningAcc * 2
-	} else if demo.turningSpeed > dt*turningAcc*2 {
-		demo.turningSpeed -= dt * turningAcc * 2
+	if rl.IsKeyDown(rl.KeyZ) {
+		up = verticalSpeed
 	}
 
-	if math.Abs(demo.turningSpeed) < dt*turningAcc {
-		demo.turningSpeed = 0
+	if rl.IsKeyDown(rl.KeyX) {
+		down = verticalSpeed
 	}
 
-	demo.updateCamera(dt)
+	if rl.IsKeyDown(rl.KeyLeft) {
+		demo.cameraAngle.X += rotationSpeed
+	}
+
+	if rl.IsKeyDown(rl.KeyRight) {
+		demo.cameraAngle.X -= rotationSpeed
+	}
+
+	if rl.IsKeyDown(rl.KeyUp) {
+		demo.cameraAngle.Y += rotationSpeed
+	}
+
+	if rl.IsKeyDown(rl.KeyDown) {
+		demo.cameraAngle.Y -= rotationSpeed
+	}
+
+	if rl.IsKeyDown(rl.KeyComma) {
+		demo.cameraAngle.Z += rotationSpeed
+	}
+
+	if rl.IsKeyDown(rl.KeyPeriod) {
+		demo.cameraAngle.Z -= rotationSpeed
+	}
+
+	demo.updateCamera(back, front, left, right, up, down)
 }
 
 func (demo *Demo) Draw() {
-	rl.SetCameraMode(demo.camera, rl.CameraCustom)
 	rl.BeginTextureMode(demo.target)
-	rl.ClearBackground(rl.RayWhite)
-
-	rl.ClearBackground(rl.DarkGray)
+	rl.ClearBackground(rl.Black)
 	rl.BeginMode3D(demo.camera)
 
-	rl.DrawPlane(rl.Vector3{5000, 0, 0}, rl.Vector2{10000, 40}, rl.LightGray)
+	rl.DrawPlane(rl.Vector3{X: 5000}, rl.Vector2{X: 10000, Y: 50}, rl.DarkGray)
 
 	columnsDrew := 0
 	for i := 0; i < maxColumns; i++ {
-		if demo.positions[i].X > demo.camera.Position.X &&
-			demo.positions[i].X-demo.camera.Position.X < demo.renderDistance {
-
+		if math.Abs(demo.positions[i].X-demo.camera.Position.X) < demo.renderDistance {
 			rl.DrawCube(demo.positions[i], 2, demo.heights[i], 2, demo.colors[i])
 			columnsDrew++
 		}
@@ -158,28 +153,45 @@ func (demo *Demo) GetTarget() rl.RenderTexture2D {
 	return demo.target
 }
 
-func (demo *Demo) updateCamera(dt float64) {
+func (demo *Demo) updateCamera(back, front, left, right, up, down float32) {
 	const (
-		sensitivity   = 1.5
 		focusDistance = 25
+		maxCameraY    = 1.48
+		minCameraY    = -1.48
+		fullRotation  = 2 * math.Pi
 	)
 
-	offsetX := demo.forwardSpeed * dt * -math.Sin(demo.cameraAngle)
-	offsetZ := demo.forwardSpeed * dt * -math.Cos(demo.cameraAngle)
-
-	demo.camera.Position.X += float32(offsetX)
-	demo.camera.Position.Z += float32(offsetZ)
-
-	demo.cameraAngle += sensitivity * dt * demo.turningSpeed
-
-	if demo.cameraAngle > 2*math.Pi {
-		demo.cameraAngle -= 2 * math.Pi
-	} else if demo.cameraAngle < 2*math.Pi {
-		demo.cameraAngle += 2 * math.Pi
+	if demo.cameraAngle.Y > maxCameraY {
+		demo.cameraAngle.Y = maxCameraY
+	} else if demo.cameraAngle.Y < minCameraY {
+		demo.cameraAngle.Y = minCameraY
 	}
 
-	demo.camera.Target.X = demo.camera.Position.X - float32(math.Sin(demo.cameraAngle)*focusDistance)
-	demo.camera.Target.Z = demo.camera.Position.Z - float32(math.Cos(demo.cameraAngle)*focusDistance)
+	if demo.cameraAngle.X > fullRotation {
+		demo.cameraAngle.X -= fullRotation
+	} else if demo.cameraAngle.X < -fullRotation {
+		demo.cameraAngle.X += fullRotation
+	}
+
+	demo.camera.Position.X +=
+		math.Sin(demo.cameraAngle.X)*back -
+			math.Sin(demo.cameraAngle.X)*front -
+			math.Cos(demo.cameraAngle.X)*left +
+			math.Cos(demo.cameraAngle.X)*right
+
+	demo.camera.Position.Y +=
+		math.Sin(demo.cameraAngle.Y)*front -
+			math.Sin(demo.cameraAngle.Y)*back + up - down
+
+	demo.camera.Position.Z +=
+		math.Cos(demo.cameraAngle.X)*back -
+			math.Cos(demo.cameraAngle.X)*front +
+			math.Sin(demo.cameraAngle.X)*left -
+			math.Sin(demo.cameraAngle.X)*right
+
+	demo.camera.Target.X = demo.camera.Position.X - math.Sin(demo.cameraAngle.X)*focusDistance
+	demo.camera.Target.Y = demo.camera.Position.Y + math.Sin(demo.cameraAngle.Y)*focusDistance
+	demo.camera.Target.Z = demo.camera.Position.Z - math.Cos(demo.cameraAngle.X)*focusDistance
 
 	dir := rl.Vector3{
 		X: (demo.camera.Target.X - demo.camera.Position.X) / focusDistance,
@@ -187,9 +199,9 @@ func (demo *Demo) updateCamera(dt float64) {
 		Z: (demo.camera.Target.Z - demo.camera.Position.Z) / focusDistance,
 	}
 
-	a := float32(demo.turningSpeed * math.Pi / 16)
+	alpha := demo.cameraAngle.Z * -math.Pi / 8
 
-	demo.camera.Up.X = a*dir.Z - dir.X*dir.Y
+	demo.camera.Up.X = alpha*dir.Z - dir.X*dir.Y
 	demo.camera.Up.Y = dir.X*dir.X + dir.Z*dir.Z
-	demo.camera.Up.Z = -dir.Y*dir.Z - a*dir.X
+	demo.camera.Up.Z = -dir.Y*dir.Z - alpha*dir.X
 }
