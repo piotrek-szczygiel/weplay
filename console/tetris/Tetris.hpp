9c322f3766b1cb2474bfb3e1207da4576c2e6158
Piotr Szczygieł <szczygiel.p@gmail.com>
2019-10-21 20:12:19 +0200

    Add all wall kicks and shapes

--- console/tetris/Tetris.hpp
+++ console/tetris/Tetris.hpp
@@ -1,19 +1,11 @@
 #include "../State.hpp"
 #include <raylib.h>
 
-#include "Shape.hpp"
-#include <boost/log/trivial.hpp>
-
 class Tetris final : public State {
 public:
     Tetris()
         : framebuffer_ { LoadRenderTexture(320, 240) }
     {
-        for (const auto& rotationKick : kicksJLSTZ) {
-            for (const auto& kick : rotationKick.clockwise) {
-                BOOST_LOG_TRIVIAL(debug) << kick.x << "\t" << kick.y;
-            }
-        }
     }
 
     ~Tetris() override
