0e20388ded517e385c787f5b626fa8bd2bd3b614
Piotr Szczygieł <szczygiel.p@gmail.com>
2020-01-06 16:52:00 +0100

    Attempt at split screen rendering

--- console/starship/Starship.cpp
+++ console/starship/Starship.cpp
@@ -5,82 +5,95 @@ namespace Starship {
 
 void Starship::update(const std::vector<ControllerState>& controllers)
 {
-    auto& state = controllers[0];
+    for (int i = 0; i < 2; ++i) {
+        auto& player = (i == 0) ? m_player_1 : m_player_2;
+        auto& controller = controllers[i];
 
-    if (state.buttons[8]) {
-        m_state_change = StateChange::Menu;
-        return;
+        if (controller.buttons[8]) {
+            m_state_change = StateChange::Menu;
+            return;
+        }
+
+        player.set_controls({
+            controller.buttons[0],
+            controller.buttons[1],
+            controller.buttons[4],
+            controller.buttons[5],
+            clamp(static_cast<float>(controller.roll) / 45.0F, -1.0F, 1.0F),
+        });
     }
 
-    m_ship.set_controls({
-        state.buttons[0],
-        state.buttons[1],
-        state.buttons[4],
-        state.buttons[5],
-        clamp(static_cast<float>(state.roll) / 45.0F, -1.0F, 1.0F),
-    });
-
     float dt = GetFrameTime();
-    m_ship.update(dt, m_map_size, m_columns);
+    m_player_1.update(dt, m_map_size, m_columns);
+    m_player_2.update(dt, m_map_size, m_columns);
 }
 
 void Starship::draw()
 {
     constexpr float RENDER_DISTANCE { 300.0F };
 
-    BeginTextureMode(m_framebuffer);
-    ClearBackground(BLACK);
-    BeginMode3D(m_ship.camera());
+    for (int i = 0; i < 2; ++i) {
+        auto& view = (i == 0) ? m_view_1 : m_view_2;
+        auto& player = (i == 0) ? m_player_1 : m_player_2;
 
-    DrawPlane(
-        {
-            0.0F,
-            0.0F,
-            m_map_size.z / 2.0F,
-        },
-        {
-            m_map_size.x,
-            m_map_size.z,
-        },
-        DARKBROWN);
+        BeginTextureMode(view);
+        ClearBackground(BLACK);
+        BeginMode3D(player.camera());
 
-    size_t columns_drawn {};
-    bool drawn_first_column {};
+        DrawPlane(
+            {
+                0.0F,
+                0.0F,
+                m_map_size.z / 2.0F,
+            },
+            {
+                m_map_size.x,
+                m_map_size.z,
+            },
+            DARKBROWN);
 
-    for (auto& column : m_columns) {
-        float distance = column.position().z - m_ship.position().z;
+        size_t columns_drawn {};
+        bool drawn_first_column {};
 
-        if (distance < 0.0F) {
-            continue;
-        }
+        for (auto& column : m_columns) {
+            float distance = column.position().z - player.position().z;
 
-        if (distance < RENDER_DISTANCE) {
-
-            if (distance > RENDER_DISTANCE * 0.5F) {
-                column.set_alpha(static_cast<unsigned char>(
-                    (1.0F - ((distance / RENDER_DISTANCE) - 0.5F) * 2.0F) * 255.0F));
-            } else {
-                column.set_alpha(255);
+            if (distance < 0.0F) {
+                continue;
             }
 
-            column.draw();
+            if (distance < RENDER_DISTANCE) {
 
-            ++columns_drawn;
-            drawn_first_column = true;
+                if (distance > RENDER_DISTANCE * 0.5F) {
+                    column.set_alpha(static_cast<unsigned char>(
+                        (1.0F - ((distance / RENDER_DISTANCE) - 0.5F) * 2.0F) * 255.0F));
+                } else {
+                    column.set_alpha(255);
+                }
 
-            if (columns_drawn % 100 == 0) {
-                EndMode3D();
-                BeginMode3D(m_ship.camera());
+                column.draw();
+
+                ++columns_drawn;
+                drawn_first_column = true;
+
+                if (columns_drawn % 100 == 0) {
+                    EndMode3D();
+                    BeginMode3D(player.camera());
+                }
+            } else if (drawn_first_column) {
+                break;
             }
-        } else if (drawn_first_column) {
-            break;
         }
+
+        EndMode3D();
+        EndTextureMode();
     }
 
-    EndMode3D();
+    BeginTextureMode(m_framebuffer);
+    DrawTexturePro(m_view_1.texture, m_view_source, m_view_dest_1, {}, 0.0F, WHITE);
+    DrawTexturePro(m_view_2.texture, m_view_source, m_view_dest_2, {}, 0.0F, WHITE);
     EndTextureMode();
 }
 
 RenderTexture2D Starship::framebuffer() { return m_framebuffer; }
-
 }
--- console/starship/Starship.hpp
+++ console/starship/Starship.hpp
@@ -10,7 +10,17 @@ namespace Starship {
 class Starship final : public State {
 public:
     Starship()
-        : m_framebuffer { LoadRenderTexture(GetScreenWidth(), GetScreenHeight()) }
+        : m_width { GetScreenWidth() }
+        , m_height { GetScreenHeight() }
+        , m_framebuffer { LoadRenderTexture(m_width, m_height) }
+        , m_view_1 { LoadRenderTexture(m_width, m_height / 2) }
+        , m_view_2 { LoadRenderTexture(m_width, m_height / 2) }
+        , m_view_source { 0.0F, 0.0F, static_cast<float>(m_width),
+            -static_cast<float>(m_height) / 2.0F }
+        , m_view_dest_1 { 0.0F, 0.0F, static_cast<float>(m_width),
+            static_cast<float>(m_height) / 2.0F }
+        , m_view_dest_2 { 0.0F, static_cast<float>(m_height) / 2.0F, static_cast<float>(m_width),
+            static_cast<float>(m_height) / 2.0F }
         , m_columns { Column::generate_random_columns(
               static_cast<size_t>(m_map_size.z / 10.0F), m_map_size, true) }
     {
@@ -24,13 +34,24 @@ public:
     StateChange state_change() override { return m_state_change; }
 
 private:
+    int m_width;
+    int m_height;
+
     RenderTexture2D m_framebuffer;
+    RenderTexture2D m_view_1;
+    RenderTexture2D m_view_2;
+
+    Rectangle m_view_source;
+    Rectangle m_view_dest_1;
+    Rectangle m_view_dest_2;
+
     StateChange m_state_change { StateChange::None };
 
-    Ship m_ship { { 0.0, 5.0, 5.0 }, 50.0F };
+    Ship m_player_1 { { 0.0, 5.0, 5.0 }, 50.0F };
+    Ship m_player_2 { { 0.0, 5.0, 5.0 }, 50.0F };
+
     Vector3 m_map_size { 40.0F, 30.0F, 10'000.0F };
 
     std::vector<Column> m_columns;
 };
-
 }
